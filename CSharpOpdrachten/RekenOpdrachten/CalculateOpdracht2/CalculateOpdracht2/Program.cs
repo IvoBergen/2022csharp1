06f938017aa19370591a62ae60c1f028f793e9f6
Ivo <34885@ma-web.nl>
2023-02-24 10:57:41 +0100

    twee opdr gemaakt!

---
+++ CSharpOpdrachten/RekenOpdrachten/CalculateOpdracht2/CalculateOpdracht2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace CalculateOpdracht2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //we gaan nu dingen kopen
+
+            int controllersInMandje = 0;
+
+
+            //koop 2 controllers
+            controllersInMandje += 2;
+
+
+            //hij is in voorraad snel kopen!
+            int ps5 = 0;
+
+            ps5++;
+
+            //koop 1 ps5
+
+
+            //koop de games:
+
+            int eldenRing = 0;
+            eldenRing++;
+
+            int shantae = 0;
+            shantae++;
+
+            int spongebob = 0;
+            spongebob++;
+
+
+            //hmmm weten we het zeker?
+            //haal 1 game weg met -- of -= 1
+            spongebob--;
+            //nu nog de prijs
+
+
+            double ps5Prijs = 499.99;
+            double controllerPrijs = 69.99;
+            double gameprijs = 59.99;
+
+
+            double totaalControllerPrijs = 0;// controllersInMandje keer controllerPrijs
+            totaalControllerPrijs = controllersInMandje * controllerPrijs;
+            double gamePrijs = 0;// game1 keer gamePrijs  + game2 keer gamePrijs (let op je haakjes!)
+            gamePrijs = (eldenRing * gameprijs) + (shantae * gameprijs);
+
+
+
+            double totaalPrijs = 0;// tel hier alles bij elkaar op
+            totaalPrijs = totaalControllerPrijs + ps5Prijs + gamePrijs;
+
+            //er moet nu 759.95 staan
+            Console.WriteLine(totaalPrijs);
+        }
+    }
+}
---
+++ CSharpOpdrachten/VariableOpdrachten/VariableOpdracht2/VariableOpdracht2/TwitchStream.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariableOpdracht2
+{
+    class TwitchStream
+    {
+        //maak hier class variables aan:
+        // type=string, naam: name,
+        string name = ("SmallAnt");
+        // type=string, naam: lastStreamName
+        string lastStreamName = ("Hide N Seek With Friends");
+        // type=Game, naam: streamGame
+        string StreamGame = ("Mario odyssey");
+        // type=int, naam: followers
+        int followers = 1200000;
+        // type=bool, naam: automod
+        bool automod = true;
+   
+        
+    }
+}
